61d4bfe9a424f53603e98bac6746dd4c98d9a535
Rahul Kumar Yadav <ry573870@gmail.com>
2025-02-27 14:02:45 +0530

    log in form UI  completed

--- loginform/src/App.jsx
+++ loginform/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         </div>  
 
         {/* input group */}
-        <div className=''>
+        <div className='px-8 pt-6 space-y-6'>
           <div className='w-full relative'>
           <EnvelopeIcon className='w-5 absolute left-3 top-1/2 -translate-y-1/2  h-5 text-gray-400' />
           <input
@@ -52,6 +52,63 @@ function App() {
           />
           </div>
         </div>
+
+        {/* options */}
+        <div className='px-8 pt-6 pb-4 flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <input
+              type='checkbox'
+              id='remember'
+              className='w-4 h-4 rounded border-gray-300 dark:border-slate-600 text-blue-500 focus:ring-blue-500'
+            />
+            <label htmlFor='remember' className='text-sm text-gray-600 dark:text-gray-300'>
+              Remember me
+            </label>
+          </div>
+          <div>
+            <a href='#' className='text-sm text-blue-500 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300'>
+              Forgot password?
+            </a>
+          </div>
+        </div>
+
+
+        {/* login button  */}
+        <div className='px-8 pb-6'>
+          <button className='w-full py-3 rounded-lg bg-blue-500 text-white font-bold hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700 transition-colors duration-300'>
+            Login
+          </button>
+        </div>
+        <div className='text-center text-sm text-gray-600 dark:text-gray-300'>
+          Dont have an account? <a href='#' className='text-blue-500 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300'>
+            Sign up
+          </a>
+        </div>
+
+        {/* social login  */}
+        <div className='px-8 py-6'>
+          <div className='relative flex items-center justify-center'>
+            <div className='border-t w-full dark:border-slate-600 border-gray-300'></div>
+            <span className='whitespace-nowrap dark:bg-slate-800 px-3 text-sm text-gray-500 dark:text-gray-400'>
+              Or connect with
+            </span>
+            <div className='border-t w-full dark:border-slate-600 border-gray-300'></div>
+          </div>
+
+          {/* social buttons */}
+          <div className='flex justify-center gap-4 mt-6'>
+            <button className='p-2 rounded-lg border dark:border-slate-600 border-gray-300 hover:scale-110 transition-transform duration-300 bg-white dark:bg-gray-200'>
+              <img src='https://www.svgrepo.com/show/512120/facebook-176.svg' alt='facebook' className='w-6 h-6 dark:' />
+            </button>
+            <button className='p-2 rounded-lg border dark:border-slate-600 border-gray-300 hover:scale-110 transition-transform duration-300 bg-white dark:bg-gray-200'>
+              <img src='https://www.svgrepo.com/show/506498/google.svg' alt='google' className='w-6 h-6' />
+            </button>
+            <button className='p-2 rounded-lg border dark:border-slate-600 border-gray-300 hover:scale-110 transition-transform duration-300 bg-white dark:bg-gray-200'>
+              <img src='https://www.svgrepo.com/show/512317/github-142.svg' alt='github' className='w-6 h-6' />
+            </button>
+          </div>
+        </div>
+
       </div>
     </div>
   )
@@ -59,23 +116,3 @@ function App() {
 
 export default App
 
-
-
-{/* <div className='px-8 py-6 space-y-6'>
-          <div className='w-full relative'>
-            <EnvelopeIcon className='absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400' />
-            <input 
-              type="email"
-              placeholder="Email"
-              className='w-full pl-10 pr-4 py-3 rounded-lg bg-transparent border dark:border-slate-600 border-blue-200 outline-none focus:border-blue-400 dark:focus:border-blue-500 transition-colors duration-300'
-            />
-          </div>
-          <div className='w-full relative'>
-            <LockClosedIcon className='absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400' />
-            <input 
-              type="password"
-              placeholder="Password"
-              className='w-full pl-10 pr-4 py-3 rounded-lg bg-transparent border dark:border-slate-600 border-blue-200 outline-none focus:border-blue-400 dark:focus:border-blue-500 transition-colors duration-300'
-            />
-          </div>
-        </div> */}
